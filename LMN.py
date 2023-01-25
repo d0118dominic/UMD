7c51bd746d383b478cf743629d79cb224f5192b9
Dominic Payne <d0118dominic@yahoo.com>
2023-01-25 12:11:14 -0500

    GSE -> LMN conversion

---
+++ LMN.py
@@ -0,0 +1,34 @@
+#%%
+# Basic GSE -> LMN converter
+
+import numpy as np
+
+# Common Rx Events' LMN transformations (in gse)
+lmn_0711 = np.array([
+[0.94822, -0.25506, -0.18926],
+[0.18182, 0.92451, -0.334996],
+[0.26042, 0.28324, 0.92301]])
+
+lmn_0617 = np.array([
+[0.93, 0.3, -0.2],
+[-0.27, 0.2, -0.94],
+[-0.24, 0.93, 0.27]])
+
+lmn_1016 = np.array([
+[0.3665, -0.1201, 0.9226],
+[0.5694, -0.7553, -0.3245],
+[0.7358, 0.6443, -0.2084]])
+
+lmn_0810 = np.array([
+[0.985, -0.141, 0.097],
+[0.152, 0.982, -0.109],
+[-0.080, 0.122, 0.989]])
+
+# Function to convert gse vector to lmn
+def convert_lmn(gse, lmn):
+    vec = np.zeros_like(gse)
+    for i in range(3):
+        vec[i] = np.dot(lmn[i],gse)
+    return vec
+
+#gse_new = convert_lmn(gse,lmn)
